af888f80914bc9606f9543d3cfbcb431921ebdb7
Wallentaine <Wallentaine@gmail.com>
2022-07-24 17:13:12 +0300

    d6c3/ complete purchased controller but not tested and complete user controller

--- controllers/purchasedController.js
+++ controllers/purchasedController.js
@@ -1,9 +1,29 @@
-const {Purchased} = require('../models/models')
+const {User, Film, Purchased} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class PurchasedController {
     async addPurchasedFilm(req, res, next) {
+        const {userId, filmId} = req.body
 
+        const user = await User.findOne({where: {userId}})
+
+        if (!user) return next(ApiError.internal("Что-то пошло не так!"))
+
+        const film = await Film.findOne({where: {filmId}})
+
+        if (!film) return next(ApiError.internal("Что-то пошло не так!"))
+
+        if (user.balance - film.price < 0) return next(ApiError.badRequest("У пользователя недостаточно средств!"))
+
+        const updateBalance = await User.update({balance: user.balance - film.price})
+
+        if (!updateBalance) return next(ApiError.internal("Что-то пошло не так!"))
+
+        const purchased = await Purchased.create({userId, filmId})
+
+        if (!purchased) return next(ApiError.badRequest("Что-то пошло не так!"))
+
+        return res.json(purchased)
     }
 
     async getAll(req, res, next) {
